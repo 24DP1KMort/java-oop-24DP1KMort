4e123aa7cd2d212725c1b42c8e4a6f8483091196
lilMorty18 <kmortuzans58@gmail.com>
2026-05-19 12:48:23 +0300

    Interface in a box

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,7 +1,19 @@
 package rvt;
 
+import rvt.interfaceInABox.*;
+
 public class App {
     public static void main(String[] args) {
-        // Your code goes here...
+        Box box = new Box(10);
+
+        box.add(new Book("Fyodor Dostoevsky", "Crime and Punishment", 2));
+        box.add(new Book("Robert Martin", "Clean Code", 1));
+        box.add(new Book("Kent Beck", "Test Driven Development", 0.7));
+
+        box.add(new CD("Pink Floyd", "Dark Side of the Moon", 1973));
+        box.add(new CD("Wigwam", "Nuclear Nightclub", 1975));
+        box.add(new CD("Rendezvous Park", "Closer to Being Here", 2012));
+
+        System.out.println(box);
     }
 }
